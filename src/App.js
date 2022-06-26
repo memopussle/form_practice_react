15b18b6f3bca32c4c695eb3756e7fd7dc8558e68
thugiang <xanhtham.cuc@gmail.com>
2022-06-27 11:43:37 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [formData, setFormData] = React.useState({
+    email: "",
+    password: "",
+    confirmedPassword: "",
+    newsletter: true,
+  });
+
+  function handleChange(event) { // replace add event listener
+    //  console.log(event.target.value); event.target: target DOMelement which is <input class="form-control mt-5"....>
+    setFormData((prevFormData) => {
+      const { name, value, type, checked } = event.target; //destructuring
+
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); //prevent default handler
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Email address"
+          name="email"
+          value={formData.email}
+          className="form-control mt-5"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={formData.password}
+          className="form-control mt-3"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="password"
+          placeholder="Confirmed Password"
+          name="confirmedPassword"
+          value={formData.confirmedPassword}
+          className="form-control mt-3"
+          onChange={handleChange}
+        ></input>
+        <input
+          className="mt-3 mx-2"
+          type="checkbox"
+          id="newsletter"
+          checked={formData.newsletter}
+          name="newsletter"
+          onChange={handleChange}
+        />
+        <label htmlFor="newsletter"> I want to join the newsletter</label>
+        <br />
+        <button type="submit" className="btn btn-md btn-danger mt-3 px-4">
+          Sign up
+        </button>
+      </form>
     </div>
   );
 }
